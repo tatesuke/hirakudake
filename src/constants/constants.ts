daf16a11869fadcfc612ac65f2e2a04e71c44b43
tatesuke <acousticdays@gmail.com>
2024-12-20 16:41:28 +0900

    App以外のコメント記載

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,8 @@
+/**
+ * URLのベースパス。
+ * `https://hoge.com/xx/` の `xx/`の部分。
+ * 
+ * 自アプリ内のリンクURLを組み立てるときなどに使用する。
+ * 例: location.href = `/${APP_BASE_PATH}`;
+ */
 export const APP_BASE_PATH = import.meta.env.VITE_BASE_PATH || "/";
--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 
+/**
+ * ブラウザのウィンドウタイトルを設定する。
+ * @param title タイトル
+ */
 export function useDocumentTitle(title: string): void {
   useEffect(() => {
     document.title = title;
