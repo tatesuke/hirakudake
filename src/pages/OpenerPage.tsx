8f3f4eafec8128e813619ea55d55164a23c48b50
tatesuke <acousticdays@gmail.com>
2024-12-20 15:38:33 +0900

    QrCodeコンポネント導入

--- src/pages/OpenerPage.tsx
+++ src/pages/OpenerPage.tsx
@@ -1,47 +1,55 @@
-import * as QRCode from "qrcode";
 import "./OpenerPage.scss";
-import { useEffect, useRef } from "react";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
 import { useQueryParameters } from "../hooks/useQueryParameters";
 import { APP_BASE_PATH } from "../constants/constants";
+import { QrCode } from "../components/QrCode";
 
+/**
+ * 指定されたURLを開くボタンを大きく表示するページ。
+ * クエリストリングに依存する @see useQueryParameters
+ *
+ * クエリストリングの検証は他の場所で行われていることを前提とする。
+ */
 export function OpenerPage() {
   const { url, title } = useQueryParameters();
   useDocumentTitle(`${title}を開く`);
 
-  const qrCanvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    (async () => {
-      if (!qrCanvasRef.current) {
-        return;
-      }
-      await QRCode.toCanvas(qrCanvasRef.current, location.href);
-    })();
-  }, [qrCanvasRef]);
-
-  const open = () => {
-    window.open(url, "noreferrer");
+  // ボタンが押されたらURLを開いて自分は閉じる
+  const onOpenButtonClick = () => {
+    window.open(url, "_blank", "noopener,noreferrer");
+    
+    // スマホでブラウザにページがたまらないように画面を閉じる。
+    // PCでは閉じないが、PC向けでないのでそれで構わない
     window.close();
   };
 
   return (
     <div className="OpenerPage">
+      {/* ヘッダ */}
       <header>
         <h1>ひらくだけ</h1>
       </header>
+
+      {/* ボタンエリア */}
       <div className="OpenerPage__main-button-area">
-        <button className="OpenerPage__main-button" onClick={open}>
+        {/* ユーザが探すまでもなく押せるくらい大きいボタン */}
+        <button className="OpenerPage__main-button" onClick={onOpenButtonClick}>
           {title}
           <br />
           を開く
         </button>
-        <a href={url} target="_blank">{url}</a>
+        <a href={url} target="_blank">
+          {url}
+        </a>
       </div>
+
+      {/* QRコード。スマホでもすぐにアクセスできるように */}
       <div>
         <p>このページは以下のQRコードからも開けます</p>
-        <canvas ref={qrCanvasRef} />
+        <QrCode data={location.href} />
       </div>
+
+    {/* 戻るボタン */}
       <div>
         <a href={`/${APP_BASE_PATH}`}>戻る</a>
       </div>
